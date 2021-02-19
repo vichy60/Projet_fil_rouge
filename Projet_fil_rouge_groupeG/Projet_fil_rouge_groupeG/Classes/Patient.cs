32da5e3f542348e5aec7ba9a72ba91513b36d21f
yannickKPL <y.cappelle@orange.fr>
2021-02-19 16:54:09 +0100

    creation solution projet fil rouge

---
+++ Projet_fil_rouge_groupeG/Projet_fil_rouge_groupeG/Classes/Patient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet_fil_rouge_groupeG.Classes
+{
+    class Patient
+    {
+    }
+}
---
+++ Projet_fil_rouge_groupeG/Projet_fil_rouge_groupeG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projet_fil_rouge_groupeG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
