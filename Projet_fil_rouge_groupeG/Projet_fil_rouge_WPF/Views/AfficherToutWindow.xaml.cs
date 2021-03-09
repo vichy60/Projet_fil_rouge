226c3f608b08c9f571b6e5b54d39ff3dd5150ac6
yannickKPL <y.cappelle@orange.fr>
2021-03-09 19:44:21 +0100

    creation fenetre principal et formulaire tout

---
+++ Projet_fil_rouge_groupeG/Projet_fil_rouge_WPF/Views/AfficherToutWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Projet_fil_rouge_WPF.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour AfficherToutWindow.xaml
+    /// </summary>
+    public partial class AfficherToutWindow : Window
+    {
+        public AfficherToutWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
